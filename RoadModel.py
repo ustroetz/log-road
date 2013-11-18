a478d9f65f19764dda39fcb7c25fc25ea7d61036
Ulrich Stroetz <ustroetz@gmail.com>
2013-11-18 13:32:00 -0800

    clean up

--- RoadModel.py
+++ RoadModel.py
@@ -4,94 +4,6 @@ import numpy as np
 from skimage.graph import route_through_array
 import requests
 
-def reprojectToWGS84(standsfn):  # creates 'standsWGS.geojson'
-    if os.path.exists('standsWGS.geojson'):
-          os.remove('standsWGS.geojson')
-    command = "ogr2ogr -f GeoJSON -t_srs EPSG:4326 %s %s" %('standsWGS.geojson', standsfn)
-    os.system(command)
-    
-def osmRoadsAPI():
-    extent =  getBbox('standsWGS.geojson')
-    bboxCoords = str(extent[0]) + ',' + str(extent[2]) + ',' + str(extent[1]) + ',' + str(extent[3])
-
-    url = 'http://www.overpass-api.de/api/xapi?way[highway=*][bbox=%s]' % bboxCoords
-    osm = requests.get(url)
-
-    # write osm_data to osm file
-    file = open(r'OSMroads.osm', 'w')
-    file.write(osm.text)
-    file.close()
-
-def osm2geojson():
-    roadsDs = ogr.Open('OSMroads.osm')
-    inLayer = roadsDs.GetLayer(1) # layer 1 for ways
-
-    outDriver = ogr.GetDriverByName('GeoJSON')
-    
-    if os.path.exists('OSMroads.geojson'):
-        outDriver.DeleteDataSource('OSMroads.geojson')
-
-    outDataSource = outDriver.CreateDataSource('OSMroads.geojson')
-    outLayer = outDataSource.CreateLayer('OSMroads.geojson', geom_type=ogr.wkbLineString )
-
-    # create the input SpatialReference
-    sourceSR = inLayer.GetSpatialRef()
-
-    # create the output SpatialReference
-    targetSR = osr.SpatialReference()
-    targetSR.ImportFromWkt('PROJCS["Albers Equal Area",GEOGCS["grs80",DATUM["unknown",SPHEROID["Geodetic_Reference_System_1980",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["standard_parallel_1",43],PARAMETER["standard_parallel_2",48],PARAMETER["latitude_of_center",34],PARAMETER["longitude_of_center",-120],PARAMETER["false_easting",600000],PARAMETER["false_northing",0],UNIT["Meter",1]]')
-
-    # create transform
-    coordTrans = osr.CoordinateTransformation(sourceSR,targetSR)
-
-    # Get the output Layer's Feature Definition
-    featureDefn = outLayer.GetLayerDefn()
-
-    # loop through the input features
-    inFeature = inLayer.GetNextFeature()
-    while inFeature:
-
-        # get the input geometry
-        geom = inFeature.GetGeometryRef()
-        # reproject the geometry
-        geom.Transform(coordTrans)
-        
-        # create a new feature
-        outFeature = ogr.Feature(featureDefn)
-
-        # set new geometry
-        outFeature.SetGeometry(geom)
-        # Add new feature to output Layer
-        outLayer.CreateFeature(outFeature)
-
-        # destroy the features and get the next input feature
-        outFeature.Destroy
-        inFeature.Destroy
-        inFeature = inLayer.GetNextFeature()
-
-    # Close DataSources
-    roadsDs.Destroy()
-    outDataSource.Destroy()
-
-def geojson2geotiff(rasterfn,bbox):    
-    xmin,xmax,ymin,ymax = bbox
-    xoff, yoff, xsize, ysize, pixelWidth, pixelHeight = bbox2pixelOffset(rasterfn,bbox)
-    
-    source_ds = ogr.Open('OSMroads.geojson')
-    source_layer = source_ds.GetLayer()
-    
-    target_ds = gdal.GetDriverByName('GTiff').Create('OSMroads.tif', xsize, ysize, gdal.GDT_Byte)
-    target_ds.SetGeoTransform((xmin, pixelWidth, 0, ymax, 0, pixelHeight))
-    band = target_ds.GetRasterBand(1)
-    NoData_value = 255
-    band.SetNoDataValue(NoData_value)
-    band.FlushCache()
-    gdal.RasterizeLayer(target_ds, [1], source_layer, burn_values=[1])   
-
-    target_dsSRS = osr.SpatialReference()
-    target_dsSRS.ImportFromWkt('PROJCS["Albers Equal Area",GEOGCS["grs80",DATUM["unknown",SPHEROID["Geodetic_Reference_System_1980",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["standard_parallel_1",43],PARAMETER["standard_parallel_2",48],PARAMETER["latitude_of_center",34],PARAMETER["longitude_of_center",-120],PARAMETER["false_easting",600000],PARAMETER["false_northing",0],UNIT["Meter",1]]')
-    target_ds.SetProjection(target_dsSRS.ExportToWkt())
-
 def buffer(standsfn, bufferfn, Dist=250):
     stands = ogr.Open(standsfn)
     lyrStands = stands.GetLayer()
@@ -112,8 +24,7 @@ def buffer(standsfn, bufferfn, Dist=250):
         outFeature.Destroy()
         featureStand.Destroy()
         featureStand = lyrStands.GetNextFeature()
-    
- 
+
 def selectCell(gridfn,bufferfn):
     grid = ogr.Open(gridfn)
     lyrGrid = grid.GetLayer()
@@ -142,7 +53,109 @@ def selectCell(gridfn,bufferfn):
 
     selectedCell = max(standDict, key=lambda x:len(standDict[x]))
     return selectedCell
+def osm2tif(standsfn,bbox):      
+    def reprojectToWGS84(standsfn,standsWGSfn):  # creates 'standsWGS.geojson'
+        if os.path.exists(standsWGSfn):
+              os.remove(standsWGSfn)
+        command = "ogr2ogr -f GeoJSON -t_srs EPSG:4326 %s %s" %(standsWGSfn, standsfn)
+        os.system(command)
+    
+    def osmRoadsAPI(standsWGSfn):
+        extent =  getBbox(standsWGSfn)
+        bboxCoords = str(extent[0]) + ',' + str(extent[2]) + ',' + str(extent[1]) + ',' + str(extent[3])
 
+        url = 'http://www.overpass-api.de/api/xapi?way[highway=*][bbox=%s]' % bboxCoords
+        osm = requests.get(url)
+
+        # write osm_data to osm file
+        file = open(r'OSMroads.osm', 'w')
+        file.write(osm.text)
+        file.close()
+
+    def osm2geojson(osmRoadsGeoJSONfn):
+        roadsDs = ogr.Open('OSMroads.osm')
+        inLayer = roadsDs.GetLayer(1) # layer 1 for ways
+
+        outDriver = ogr.GetDriverByName('GeoJSON')
+    
+        if os.path.exists(osmRoadsGeoJSONfn):
+            outDriver.DeleteDataSource(osmRoadsGeoJSONfn)
+
+        outDataSource = outDriver.CreateDataSource(osmRoadsGeoJSONfn)
+        outLayer = outDataSource.CreateLayer(osmRoadsGeoJSONfn, geom_type=ogr.wkbLineString )
+
+        # create the input SpatialReference
+        sourceSR = inLayer.GetSpatialRef()
+
+        # create the output SpatialReference
+        targetSR = osr.SpatialReference()
+        targetSR.ImportFromWkt('PROJCS["Albers Equal Area",GEOGCS["grs80",DATUM["unknown",SPHEROID["Geodetic_Reference_System_1980",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["standard_parallel_1",43],PARAMETER["standard_parallel_2",48],PARAMETER["latitude_of_center",34],PARAMETER["longitude_of_center",-120],PARAMETER["false_easting",600000],PARAMETER["false_northing",0],UNIT["Meter",1]]')
+
+        # create transform
+        coordTrans = osr.CoordinateTransformation(sourceSR,targetSR)
+
+        # Get the output Layer's Feature Definition
+        featureDefn = outLayer.GetLayerDefn()
+
+        # loop through the input features
+        inFeature = inLayer.GetNextFeature()
+        while inFeature:
+
+            # get the input geometry
+            geom = inFeature.GetGeometryRef()
+            # reproject the geometry
+            geom.Transform(coordTrans)
+        
+            # create a new feature
+            outFeature = ogr.Feature(featureDefn)
+
+            # set new geometry
+            outFeature.SetGeometry(geom)
+            # Add new feature to output Layer
+            outLayer.CreateFeature(outFeature)
+
+            # destroy the features and get the next input feature
+            outFeature.Destroy
+            inFeature.Destroy
+            inFeature = inLayer.GetNextFeature()
+
+        # Close DataSources
+        roadsDs.Destroy()
+        outDataSource.Destroy()
+
+    def geojson2geotiff(rasterfn,bbox,osmRoadsGeoJSONfn,osmRoadsTiffn):    
+        xmin,xmax,ymin,ymax = bbox
+        xoff, yoff, xsize, ysize, pixelWidth, pixelHeight = bbox2pixelOffset(rasterfn,bbox)
+    
+        source_ds = ogr.Open(osmRoadsGeoJSONfn)
+        source_layer = source_ds.GetLayer()
+    
+        target_ds = gdal.GetDriverByName('GTiff').Create(osmRoadsTiffn, xsize, ysize, gdal.GDT_Byte)
+        target_ds.SetGeoTransform((xmin, pixelWidth, 0, ymax, 0, pixelHeight))
+        band = target_ds.GetRasterBand(1)
+        NoData_value = 255
+        band.SetNoDataValue(NoData_value)
+        band.FlushCache()
+        gdal.RasterizeLayer(target_ds, [1], source_layer, burn_values=[1])   
+
+        target_dsSRS = osr.SpatialReference()
+        target_dsSRS.ImportFromWkt('PROJCS["Albers Equal Area",GEOGCS["grs80",DATUM["unknown",SPHEROID["Geodetic_Reference_System_1980",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["standard_parallel_1",43],PARAMETER["standard_parallel_2",48],PARAMETER["latitude_of_center",34],PARAMETER["longitude_of_center",-120],PARAMETER["false_easting",600000],PARAMETER["false_northing",0],UNIT["Meter",1]]')
+        target_ds.SetProjection(target_dsSRS.ExportToWkt())
+        
+    def main(standsfn,bbox):
+        standsWGSfn = 'standsWGS.geojson'
+        osmRoadsGeoJSONfn = 'OSMroads.geojson'
+        osmRoadsTiffn = 'OSMroads.tif'
+        reprojectToWGS84(standsfn,standsWGSfn)  # creates 'standsWGS.geojson'
+        osmRoadsAPI(standsWGSfn) # creates 'roads.osm'
+        osm2geojson(osmRoadsGeoJSONfn) # creates 'osmroads.geojson'
+        geojson2geotiff(costSurfacefn,bbox,osmRoadsGeoJSONfn,osmRoadsTiffn) # creates 'OSMroads.tif'
+        os.remove('standsWGS.geojson')
+        os.remove('OSMroads.osm')
+    
+    if __name__ == "__main__":
+        main(standsfn,bbox)
+     
 def getBbox(shpfn):
     ds = ogr.Open(shpfn)
     lyr = ds.GetLayer()
@@ -229,19 +242,14 @@ def main(standsfn,bufferfn,gridfn,costSurfacefn,osmRoadsfn,newCostSurfacefn):
     selectedCell = selectCell(gridfn,bufferfn) # creates string 'selectedCell'
     costSurfaceArray, bbox = raster2array(costSurfacefn,standsfn) # creates array 'costSurfaceArray' and float 'bbox'
     
-    # osm2tif 
-    reprojectToWGS84(standsfn)  # creates 'standsWGS.geojson'
-#    osmRoadsAPI() # creates 'roads.osm'
-    osm2geojson() # creates 'osmroads.geojson'
-    geojson2geotiff(costSurfacefn,bbox) # creates 'OSMroads.tif'
-    os.remove('standsWGS.geojson')
-#    os.remove('OSMroads.osm')
+    osm2tif(standsfn,bbox) # creates 'OSMroads.tif' (existing OSM roads)
     
-    osmRoadsArray, bbox = raster2array(osmRoadsfn,standsfn) # creates array 'costSurfaceArray' and float 'bbox'
+    osmRoadsArray, bbox = raster2array(osmRoadsfn,standsfn) # creates array 'osmRoadsArray' and float 'bbox'
     costSurfaceArray[osmRoadsArray == 1.0] = 0 # updates array 'costSurfaceArray'    
-    costSurfaceArray = createPath(newCostSurfacefn,costSurfaceArray,selectedCell,gridfn) # updates array 'costSurfaceArray'
+    costSurfaceArray = createPath(costSurfacefn,costSurfaceArray,selectedCell,gridfn) # updates array 'costSurfaceArray'
     
     array2raster(newCostSurfacefn,costSurfacefn,bbox,costSurfaceArray) # writes 'costSurfaceArray' to 'newCostSurface.tif'
+    
     
     
         
@@ -249,7 +257,7 @@ if __name__ == "__main__":
     standsfn = 'stands.shp'
     gridfn = 'grid.shp'
     bufferfn = 'buffer_stands.shp'
-    costSurfacefn = 'slopeBig.tif'
+    costSurfacefn = 'slope.tif'
     osmRoadsfn = 'osmRoads.tif'
     newCostSurfacefn = 'newCostSurface.tif'
     main(standsfn,bufferfn,gridfn,costSurfacefn,osmRoadsfn,newCostSurfacefn)
