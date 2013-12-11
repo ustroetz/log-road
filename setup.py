617c2cd1d1463c390d896728d1ac86a3008658f9
Ulrich Stroetz <ustroetz@gmail.com>
2013-12-11 11:13:20 -0800

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='log-road',
+      version='0.1',
+      author='Ulrich Stroetz',
+      author_email='ustroetz@gmail.com',
+      packages=['logroad'],
+     )
+	 
+	
