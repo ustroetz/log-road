4f45844e2c8c86d0411c1b25d9b1eec2045ff14f
Ulrich Stroetz <ustroetz@gmail.com>
2014-01-14 13:51:38 -0800

    modified test data

--- test_roadmodel.py
+++ test_roadmodel.py
@@ -3,7 +3,7 @@ from logroad import RoadModel as rm
 if __name__ == "__main__":
     standsfn = 'testdata/test_stand.shp'
     costSurfacefn = 'testdata/CostSurface.tif'
-    newRoadsfn = 'newRoad.shp'
+    newRoadsfn = 'testdata/newRoad.shp'
     
     rm.main(standsfn,costSurfacefn,newRoadsfn)
 
