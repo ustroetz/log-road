baa1841369aa9b9b4357120bb8e62ecd5786a290
Ulrich Stroetz <ustroetz@gmail.com>
2014-01-14 13:56:15 -0800

    add comments

--- test_costsurface.py
+++ test_costsurface.py
@@ -1,8 +1,8 @@
 from logroad import CostSurface as cs
 
 if __name__ == "__main__":
-    riverfn = 'testdata/rivers.shp'
-    slopefn = 'testdata/Slope.tif'
+    riverfn = 'testdata/rivers.shp' # EPSG32610
+    slopefn = 'testdata/Slope.tif'  # EPSG32610
      
     cs.main(riverfn, slopefn)
 
--- test_roadmodel.py
+++ test_roadmodel.py
@@ -1,8 +1,8 @@
 from logroad import RoadModel as rm
 
 if __name__ == "__main__":
-    standsfn = 'testdata/test_stand.shp'
-    costSurfacefn = 'testdata/CostSurface.tif'
+    standsfn = 'testdata/test_stand.shp'       # EPSG3857
+    costSurfacefn = 'testdata/CostSurface.tif' # EPSG32610
     newRoadsfn = 'testdata/newRoad.shp'
     
     rm.main(standsfn,costSurfacefn,newRoadsfn)
